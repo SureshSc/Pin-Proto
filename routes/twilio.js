fd29553115b13a9de3308f31fa118e881811a33d
Suresh Chandrababu <suresh.chandrababu@gyanmatrix.com>
2020-08-05 16:39:14 +0530

    Adding twilio

---
+++ routes/twilio.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+
+const VoiceResponse = require('twilio').twiml.VoiceResponse;
+
+
+router.post('/enterpin', function(req, res, next) {
+    const response = new VoiceResponse();
+    const gather = response.gather({
+        action: '/error',
+        method: 'GET',
+        numDigits: 6
+    });
+    gather.say('Enter your 6 digit pin after the message');
+    response.redirect({
+        method: 'GET'
+    }, '/twilio/result');
+    
+    console.log(response.toString());
+    res.set('Content-Type', 'text/xml');
+    res.send(response.toString())
+});
+
+
+router.get('/result', function(req, res, next) {
+    res.send('respond with a resource');
+});
+
+router.get('/error', function(req, res, next) {
+    res.send('Failed to give in put');
+});
+  
+router.get('/', function(req, res, next) {
+    res.send('Welcome to Twilio');
+});
+  
+
+module.exports = router;
